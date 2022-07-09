1252fb8e653e9a0f58ce518461cc19c54e8ec57b
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-07-09 19:50:08 +0700

    add oauth resource server

---
+++ Oauth-Resource-Server/src/main/java/rifqimuhammadaziz/oauthresourceserver/controller/UserController.java
@@ -0,0 +1,13 @@
+package rifqimuhammadaziz.oauthresourceserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/api/users")
+    public String[] getUser() {
+        return new String[]{"Rifqi", "Bagas", "Bayu"};
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Getting Started
+
+### Reference Documentation
+For further reference, please consider the following modules:
+
+* Oauth-Authorization-Server used to configure security server
+* Oauth-Resource-Server used to define resource data
+* Spring-Security-Client used to test as client
